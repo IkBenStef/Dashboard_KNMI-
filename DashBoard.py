b6d9202d0ff450b93e8c4e0d3f8b69b870d67349
Stef <66081006+IkBenStef@users.noreply.github.com>
2026-02-24 15:45:50 +0100

    Add files via upload

---
+++ DashBoard.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="KNMI Dashboard", layout="wide")
+
+st.title("📊 KNMI Temperatuur Dashboard")
+st.markdown("Gebruik het menu links om een pagina te selecteren.")

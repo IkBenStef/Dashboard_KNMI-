32e451b5dffa943d36e76df758daa42e54e7233e
Stef <66081006+IkBenStef@users.noreply.github.com>
2026-02-24 15:45:31 +0100

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="KNMI Dashboard", layout="wide")
-
-st.title("📊 KNMI Temperatuur Dashboard")
-st.markdown("Gebruik het menu links om een pagina te selecteren.")

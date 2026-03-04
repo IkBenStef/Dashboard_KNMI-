2c3a5d045fe6b5c4b7832a92c59563615f873c90
Stef <66081006+IkBenStef@users.noreply.github.com>
2026-03-04 17:27:20 +0100

    Update 2_🔘_Vergelijking_KNMI.py

--- pages/2_🔘_Vergelijking_KNMI.py
+++ pages/2_🔘_Vergelijking_KNMI.py
@@ -50,7 +50,7 @@ fig.add_trace(go.Bar(
     x=df_yearly_rain_filtered['year'],
     y=df_yearly_rain_filtered['Neerslag_MM'],
     name='Totale neerslag (mm)',
-    yaxis='y2',
+    yaxis='y1',
     opacity=0.5
 ))
 fig.add_trace(go.Scatter(
@@ -58,7 +58,7 @@ fig.add_trace(go.Scatter(
     y=df_yearly_temp_filtered['Temperatuur_C'],
     mode='lines+markers',
     name='Gem. temperatuur (°C)',
-    yaxis='y1'
+    yaxis='y2'
 ))
 fig.update_layout(
     template="plotly_white",
@@ -92,6 +92,7 @@ st.subheader("Correlatie Neerslag <-> Windsnelheid")
 
 fig_corr_wind_rian = px.scatter(df_filtered,x='Windsnelheid_ms',y='Neerslag_MM',opacity=0.7)
 st.plotly_chart(fig_corr_wind_rian, use_container_width=True)
+
 
 
 
